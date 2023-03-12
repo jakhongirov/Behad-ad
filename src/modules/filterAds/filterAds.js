b5d45e7b41b087b8a577202373774f05fe088d32
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-03-12 17:43:01 +0500

    add

--- src/modules/filterAds/filterAds.js
+++ src/modules/filterAds/filterAds.js
@@ -13,7 +13,8 @@ module.exports = {
                 const foundAd = await model.foundAd(foundUser.user_age, foundUser.user_who, foundUser.user_country, foundUser.user_capital, foundUser.user_phone_lang)
 
                 if (foundAd) {
-                    await model.addAction(app?.app_id, adId, foundAd.campaign_id, foundUser.user_id)
+                    console.log(foundAd);
+                    await model.addAction(app?.app_id, adId, foundAd?.campaign_id, foundUser.user_id)
                     return res.json({
                         status: 200,
                         message: "Success",
@@ -21,7 +22,7 @@ module.exports = {
                     })
                 } else {
                     const chooseAllAd = await model.chooseAllAd()
-                    await model.addAction(app?.app_id, adId, chooseAllAd.campaign_id, foundUser.user_id)
+                    await model.addAction(app?.app_id, adId, chooseAllAd?.campaign_id, foundUser.user_id)
 
                     return res.json({
                         status: 200,
@@ -31,7 +32,7 @@ module.exports = {
                 }
             } else {
                 const chooseAllAd = await model.chooseAllAd()
-                await model.addAction(app?.app_id, adId, chooseAllAd.campaign_id, foundUser.user_id)
+                await model.addAction(app?.app_id, adId, chooseAllAd?.campaign_id, foundUser.user_id)
 
                 return res.json({
                     status: 200,
