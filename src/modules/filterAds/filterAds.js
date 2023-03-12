349b0c64f1a6732bdf49f58cefbabcbc78218461
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-03-12 17:32:25 +0500

    add

--- src/modules/filterAds/filterAds.js
+++ src/modules/filterAds/filterAds.js
@@ -7,11 +7,13 @@ module.exports = {
             const foundUser = await model.foundUser(deviceId)
             const app = await model.foundApp(adId)
 
+            console.log(app);
+
             if (foundUser) {
                 const foundAd = await model.foundAd(foundUser.user_age, foundUser.user_who, foundUser.user_country, foundUser.user_capital, foundUser.user_phone_lang)
 
                 if (foundAd) {
-                    await model.addAction(app.app_id, adId, foundAd.campaign_id, foundUser.user_id)
+                    await model.addAction(app?.app_id, adId, foundAd.campaign_id, foundUser.user_id)
                     return res.json({
                         status: 200,
                         message: "Success",
