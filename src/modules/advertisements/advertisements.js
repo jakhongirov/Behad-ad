e97d28b1186df981105e0487ba379a6accd3ed62
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-03-15 17:15:00 +0500

    add

--- src/modules/advertisements/advertisements.js
+++ src/modules/advertisements/advertisements.js
@@ -400,7 +400,7 @@ module.exports = {
             if (uploadPhoto) {
                 deleteOldLogo.delete()
                 image_name = uploadPhoto.filename
-                image_url = `https://psychology.behad.uz/public/images/${uploadPhoto.filename}`
+                image_url = `https://ads.behad.uz/public/images/${uploadPhoto.filename}`
             } else {
                 image_url = foundAd?.advertisement_media_link
                 image_name = foundAd?.advertisement_media_name
