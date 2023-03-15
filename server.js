d042aa5f460843a0a85272238e2bf272df7340d9
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-03-15 17:58:28 +0500

    add

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const app = express();
 const { PORT } = require("./src/config");
 const router = require("./src/modules");
 
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(path.resolve(__dirname, 'public')))
-app.use(express.json({limit: "10mb", extended: true}))
-app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}))
 app.use("/api/v1", router);
 
 app.listen(PORT, console.log(PORT));
